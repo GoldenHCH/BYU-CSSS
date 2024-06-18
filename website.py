fd435013ac635382512dc5b885b7a91938f764f6
Chenhao Huang <109107564+GoldenHCH@users.noreply.github.com>
2024-06-18 15:09:11 -0600

    created person class

--- website.py
+++ website.py
@@ -1,9 +1,29 @@
-from flask import Flask
-import pandas as pd
+from flask import FLask, render_template, request
+import flask_sqlalchemy import SQLAlchemy
 
-df = pd.DataFrame(columns =['Name', 'Graduation', 'Residence', 'Major', 'Industry']
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_']
 
-@app.route("/")
+class person:
+    def __init__(self, first_name, last_name, dob, major, location, company=None, email, wechat):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.major = major
+        self.location = location
+        self.company = company
+        self.email = email
+        self.wechat = wechat
 
-def
+    def update(self, first_name, last_name, dob, major, location, company=None, email, wechat)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.major = major
+        self.location = location
+        self.company = company
+        self.email = email
+        self.wechat = wechat
+
+
+    
